da324c825471df762e02a487029246edd0e70a26
William Angelo Gonzales <williamangeloviverogonzales@gmail.com>
2026-08-18 16:25:15 +0800

    Use path.resolve for static files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json({ limit: '2mb' }));
 app.use(cors());
-app.use(express.static(path.join(__dirname))); 
+app.use(express.static(path.resolve(__dirname)));
 
 // Supabase Configuration
 const SUPABASE_URL = "https://fkeujqzgqupvjcqreqcs.supabase.co";
